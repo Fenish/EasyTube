966a2f4f4fae3c3822ce8d26b07a8c8f59f99c83
fenish <sohretalhadev@gmail.com>
2023-04-28 23:24:42 +0300

    remove debug code

--- server/api/convert.ts
+++ server/api/convert.ts
@@ -5,15 +5,7 @@ export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   const videoUrl = query.url;
   const video = await ytdl.getInfo(videoUrl as string);
-  console.log("bitti");
   return {
     video,
   };
-  // const runJob = pProgress(async (progress) => {
-  //   await ytdl.getInfo(videoUrl as string);
-  // });
-  // runJob.onProgress(console.log);
-  // return {
-  //   test: runJob,
-  // };
 });
