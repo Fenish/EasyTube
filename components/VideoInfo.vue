6b62cbc8ed37dccbc559b78fda792b4e55969ace
fenish <sohretalhadev@gmail.com>
2023-05-02 21:45:01 +0300

    add progress bar and download button

--- components/VideoInfo.vue
+++ components/VideoInfo.vue
@@ -17,17 +17,29 @@ function formatTime(totalSeconds: number): string {
   let minutes: number = Math.floor((totalSeconds - hours * 3600) / 60);
   let seconds: number = totalSeconds - hours * 3600 - minutes * 60;
 
-  // Use string interpolation to format the time
   return `${hours.toString().padStart(2, "0")}:${minutes
     .toString()
     .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
+
+function formatNumber(num: number) {
+  if (num >= 1000000000) {
+    return (num / 1000000000).toFixed(1).replace(/\.0$/, "") + "B";
+  }
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+  }
+  return num ? num : 0;
 }
 </script>
 
 <template>
   <div class="flex flex-col">
     <div
-      class="flex flex-col bg-zinc-800 border-zinc-700 border-2 select-none rounded-sm w-72 md:w-96"
+      class="flex flex-col bg-zinc-800 border-zinc-700 border-2 select-none rounded-sm w-72 md:w-[500px]"
     >
       <img
         class="w-full h-auto mx-auto"
@@ -56,28 +68,38 @@ function formatTime(totalSeconds: number): string {
               {{ formatTime(videoData.videoDetails.lengthSeconds) }}
             </a>
           </div>
-          <div class="flex mt-4">
-            <div class="flex items-center mr-5">
-              <Icon
-                name="ic:baseline-remove-red-eye"
-                class="text-zinc-500 w-5 h-5 mr-1"
-              />
-              <a class="text-zinc-500">
-                {{ videoData.videoDetails.viewCount }}
-              </a>
+          <div class="flex mt-4 flex-col md:flex-row">
+            <div class="flex">
+              <div class="flex items-center mr-5">
+                <Icon
+                  name="ic:baseline-remove-red-eye"
+                  class="text-zinc-500 w-5 h-5 mr-1"
+                />
+                <a class="text-zinc-500">
+                  {{ formatNumber(videoData.videoDetails.viewCount) }}
+                </a>
+              </div>
+              <div class="flex items-center">
+                <Icon
+                  name="ion:md-thumbs-up"
+                  class="text-zinc-500 w-5 h-5 mr-1"
+                />
+                <a class="text-zinc-500">
+                  {{ formatNumber(videoData.videoDetails.likes) }}
+                </a>
+              </div>
             </div>
-            <div class="flex items-center">
-              <Icon
-                name="ion:md-thumbs-up"
-                class="text-zinc-500 w-5 h-5 mr-1"
-              />
-              <a class="text-zinc-500">
-                {{ videoData.videoDetails.likes }}
-              </a>
+            <div class="flex pb-2 w-full justify-end">
+              <button
+                class="bg-violet-500 text-white px-5 py-2 rounded-md w-full mt-3 md:mt-0 md:w-auto"
+              >
+                Download {{ format }}
+              </button>
             </div>
           </div>
         </div>
       </div>
+      <ProgressBar :barWidth="0" />
     </div>
   </div>
 </template>
