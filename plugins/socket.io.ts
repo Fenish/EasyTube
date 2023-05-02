9506b96197c90c5775a71edcfa0256c57011a589
fenish <sohretalhadev@gmail.com>
2023-05-02 21:47:29 +0300

    remove unused variable

--- plugins/socket.io.ts
+++ plugins/socket.io.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig().public;
   const socket = io(config.socketUrl + ":" + config.socketPort, {
     autoConnect: false,
