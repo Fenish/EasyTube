5d6a0942c95166833a38e1b1755f41ed599e22be
fenish <sohretalhadev@gmail.com>
2023-05-02 21:45:21 +0300

    remove text indicator from progress bar

--- components/ProgressBar.vue
+++ components/ProgressBar.vue
@@ -8,12 +8,11 @@ defineProps({
 </script>
 
 <template>
-  <div class="w-full bg-gray-700 rounded-full">
+  <div class="w-full bg-gray-700">
     <div
-      class="bg-purple-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full transition-all duration-500 ease-in-out"
+      class="bg-purple-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none transition-all duration-500 ease-in-out"
       :style="`width: ${barWidth}%`"
-    >
-      {{ barWidth }}%
-    </div>
+      v-if="barWidth !== 0"
+    ></div>
   </div>
 </template>
