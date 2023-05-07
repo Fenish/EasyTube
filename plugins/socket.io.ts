8a44c3c36cdd2c1bfa1c6deb1f3d3da4e1e79161
fenish <sohretalhadev@gmail.com>
2023-05-07 13:55:34 +0300

    rename variable

--- plugins/socket.io.ts
+++ plugins/socket.io.ts
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig().public;
-  const socket = io(config.socketUrl + ":" + config.socketIOPort, {
+  const socket = io(config.socketUrl + ":" + config.socketPort, {
     autoConnect: false,
   });
 
